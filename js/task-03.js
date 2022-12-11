22039a39f9adc3649a40b7ef777e10447504088b
Yulia-Safonova-85 <109626421+Yulia-Safonova-85@users.noreply.github.com>
2022-12-11 16:46:12 +0200

    task_03

---
+++ js/task-03.js
@@ -0,0 +1,31 @@
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. 
+// Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+   
+const listGallery = document.querySelector('.gallery');
+
+listGallery.style.listStyle = "none";
+listGallery.style.display = "flex";
+listGallery.style.justifyContent = "space-around";
+
+const markup = images.map(({ url, alt }) =>
+  `<li> <img src="${url}" alt ="${alt} width ="200" height = "200"></li>`).join('')
+  listGallery.insertAdjacentHTML('beforeend', markup)
+  console.log(markup); 
